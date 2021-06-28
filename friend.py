4a0fd96eff553188b86ba77e8bc3b9f70589cc2a
arshedgithub <arshedahmedmail@gmail.com>
2021-06-28 11:43:58 +0530

    bug fixing

--- friend.py
+++ friend.py
@@ -7,14 +7,16 @@ friend.say(f"Hi {name}, i'm david")
 friend.runAndWait()
 
 while True:
-    friend.say("Type something")
+    speach = "Type something : "
+
+    friend.say(speach)
     friend.runAndWait()
 
-    c = input("Type something : ")
+    c = input(speach)
     friend.say(c)
     friend.runAndWait()
 
-    if input("Do you want to Quit ? Y/N ") == 'y':
+    if input("Do you want to Quit ? Y/N \n") == 'y' or 'Y':
         friend.say("good bye, see you later")
         friend.runAndWait()
         break
