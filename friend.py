d30c4b6c8815aba62cfe5a52eeb93a1e0c6fa880
arshedgithub <arshedahmedmail@gmail.com>
2021-06-28 11:43:58 +0530

    bug fixing

--- friend.py
+++ friend.py
@@ -3,18 +3,25 @@ import pyttsx3
 name = input("Enter your Name : ")
 
 friend = pyttsx3.init()
-friend.say(f"Hi {name}, i'm david")
+friend.say(f"Hi {name}, i'm david, How can I help you?")
 friend.runAndWait()
 
+options = "Enter 1 for read pdf : "
+friend.say(options)
+friend.runAndWait()
+input(options)
+
 while True:
-    friend.say("Type something")
+    speach = "Type something : "
+
+    friend.say(speach)
     friend.runAndWait()
 
-    c = input("Type something : ")
+    c = input(speach)
     friend.say(c)
     friend.runAndWait()
 
-    if input("Do you want to Quit ? Y/N ") == 'y':
+    if input("Do you want to Quit ? Y/N \n") == 'y' or 'Y':
         friend.say("good bye, see you later")
         friend.runAndWait()
         break
